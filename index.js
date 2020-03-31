229536110e736e79d5a8f4116ac82616538c2026
mougenko <mougenko@gmail.com>
2020-03-31 15:38:54 +0900

    test

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 
 async function run() {
   try {
-    console.log(core.getInput('github-token'))
+    //console.log(core.getInput('github-token'))
+    console.log(github.context.payload);
+    console.log(github.context.repo.owner);
+    console.log(github.context.repo.repo);
+
+
   } 
   catch (error) {
     core.setFailed(error.message);
